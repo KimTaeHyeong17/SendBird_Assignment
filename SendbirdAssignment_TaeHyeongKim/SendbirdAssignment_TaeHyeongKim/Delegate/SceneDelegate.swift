f4492042344f50c8b47d7bbd5e3e6dd43be0563b
TaeHyeong Kim <uuzaza@naver.com>
2021-02-15 15:32:55 +0900

    regroup files, add navigationController to rootview

--- SendbirdAssignment_TaeHyeongKim/SendbirdAssignment_TaeHyeongKim/Delegate/SceneDelegate.swift
+++ SendbirdAssignment_TaeHyeongKim/SendbirdAssignment_TaeHyeongKim/Delegate/SceneDelegate.swift
@@ -16,7 +16,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(windowScene: windowScene)
+        //self.window =  UIWindow(frame: UIScreen.main.bounds)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let rootVC = storyboard.instantiateViewController(identifier: "SearchViewController") as? SearchViewController else {
+            print("ViewController not found")
+            return
+        }
+        let rootNavigationController = UINavigationController(rootViewController: rootVC)
+        self.window?.rootViewController = rootNavigationController
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
